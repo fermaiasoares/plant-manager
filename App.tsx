f87c70b295aa586583d4b3d5b6188f7b3b186e13
Fernando Maia <fermaiasoares@aol.com>
2021-04-21 18:01:10 -0300

    feat: :rocket: Criando a Navegação
    Criando o Conteiner de navegação em pilhas

--- App.tsx
+++ App.tsx
@@ -1,10 +1,33 @@
+import 'react-native-gesture-handler';
 import React from 'react';
+import AppLoading from 'expo-app-loading';
+import { 
+  useFonts, 
+  Jost_400Regular,
+  Jost_600SemiBold,
+  Jost_500Medium
+} from '@expo-google-fonts/jost';
 
-import Welcome from './src/pages/Welcome';
+import AppRoutes from './src/routes/stack.routes';
+import { NavigationContainer } from '@react-navigation/native';
 
 const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold,
+    Jost_500Medium
+  });
+
+  if(!fontsLoaded) {
+    return (
+      <AppLoading />
+    )
+  }
+
   return (
-    <Welcome />
+    <NavigationContainer>
+      <AppRoutes />
+    </NavigationContainer>
   )
 }
 
