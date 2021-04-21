dd4c946936c641607a4ee3892f2f4049af39ff61
Fernando Maia <fermaiasoares@aol.com>
2021-04-21 17:58:17 -0300

    refactor: :construction_worker: Refatorando Welcome
    Refatorando o Código para novas adaptações

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,51 +1,99 @@
-import React, { useEffect, useState } from 'react';
-import { SafeAreaView, Text, Image, StyleSheet } from 'react-native';
+import React from 'react';
+import { useNavigation } from '@react-navigation/core';
+import { 
+  SafeAreaView, 
+  Text, 
+  Image, 
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+  View
+} from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
-import Button from '../components/Button';
 import colors from '../../styles/colors';
+import fonts from '../../styles/fonts';
 import watering from '../assets/watering.png';
 
-const pages: React.FC = () => {
-  
+const Welcome: React.FC = () => {
+  const navigation = useNavigation();
+
   return (
     <SafeAreaView style={style.container}>
-      <Text style={style.title}>
-        Gerencie {'\n'}
-        suas plantas {'\n'} 
-        de forma fácil</Text>
+      <View style={style.wrapper}>
+        <Text style={style.title}>
+          Gerencie{'\n'}
+          suas plantas de{'\n'} 
+          forma fácil</Text>
 
-      <Image source={watering} style={style.image} />
+        <Image 
+          source={watering} 
+          style={style.image}
+          resizeMode="contain" 
+          />
 
-      <Text style={style.subTitle}>Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você sempre que precisar.</Text>
+        <Text style={style.subTitle}>
+          Não esqueça mais de regar suas plantas.
+          Nós cuidamos de lembrar você sempre que precisar.
+        </Text>
 
-      <Button title=">"/>
+        <TouchableOpacity 
+          style={style.button}
+          activeOpacity={0.7}
+          onPress={() => navigation.navigate('UserIdentification')}
+          >
+          <Feather 
+            name="chevron-right" 
+            style={style.buttonIcon}
+            />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
 
 const style = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  wrapper: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-between'
+    justifyContent: 'space-around',
+    paddingHorizontal: 20,
   },
   title: {
     marginTop: 38,
+    fontFamily: fonts.heading,
     fontSize: 32,
-    fontWeight: 'bold',
     textAlign: 'center',
+    lineHeight: 38,
     color: colors.heading
   },
   subTitle: {
+    fontFamily: fonts.text,
+    fontSize: 17,
     textAlign: 'center',
-    fontSize: 18,
+    lineHeight: 25,
     paddingHorizontal: 20,
     color: colors.heading
   },
   image: {
-    width: 292,
-    height: 284
-  }
+    height: Dimensions.get('window').width * 0.7
+  },
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56
+  },
+  buttonIcon: {
+    color: colors.white,
+    fontSize: 24,
+  },
 })
 
-export default pages;
+export default Welcome;
